34c346497b5aa356f08e6168616a84718348d844
Calle Laakkonen <calle@luolamies.org>
2011-04-20 20:02:08 +0300

    Cut outline now cuts subpaths in ascending order.

--- src/org/luolamies/jgcgen/shapes/outline/Cut.java
+++ src/org/luolamies/jgcgen/shapes/outline/Cut.java
@@ -1,6 +1,10 @@
 package org.luolamies.jgcgen.shapes.outline;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.IdentityHashMap;
 import java.util.Iterator;
+import java.util.List;
 
 import org.luolamies.jgcgen.RenderException;
 import org.luolamies.jgcgen.path.Axis;
@@ -16,6 +20,7 @@ public class Cut implements PathGenerator {
 	
 	private double z0, depth, passdepth;
 	private double ramplen;
+	private boolean finish = true;
 	
 	public Cut src(PathGenerator path) {
 		src = path;
@@ -42,6 +47,11 @@ public class Cut implements PathGenerator {
 		return this;
 	}
 	
+	public Cut noFinish() {
+		finish = false;
+		return this;
+	}
+	
 	@Override
 	public Path toPath() {
 		if(this.src==null)
@@ -50,9 +60,62 @@ public class Cut implements PathGenerator {
 			throw new RenderException("Depth not set!");
 		if(passdepth<=0)
 			throw new RenderException("Pass depth not set!");
+
+		final List<Path> sources = src.toPath().splitAtSubpaths();
+		// Sort the paths from smallest to largest, so the small (possibly inner) parts
+		// get cut first.
+		if(sources.size()>1) {
+			Collections.sort(sources, new Comparator<Path>() {
+				IdentityHashMap<Path, Double> areacache = new IdentityHashMap<Path, Double>(sources.size());
+				
+				@Override
+				public int compare(Path p1, Path p2) {
+					double a1 = calcArea(p1);
+					double a2 = calcArea(p2);
+					return Double.compare(a1, a2);
+				}
+				
+				/** Calculate the area of a path bounding rectangle */
+				private double calcArea(Path p) {
+					if(areacache.containsKey(p))
+						return areacache.get(p);
+					
+					double minx=Double.MAX_VALUE, maxx=-Double.MAX_VALUE;
+					double miny=Double.MAX_VALUE, maxy=-Double.MAX_VALUE;
+					for(Path.Segment s : p.getSegments()) {
+						if(s.point!=null) {
+							Double x = ((NumericCoordinate)s.point).getValue(Axis.X);
+							Double y = ((NumericCoordinate)s.point).getValue(Axis.Y);
+							if(x!=null) {
+								if(x<minx)
+									minx = x;
+								if(x>maxx)
+									maxx = x;
+							}
+							if(y!=null) {
+								if(y<miny)
+									miny = y;
+								if(y>maxy)
+									maxy = y;
+							}
+						}
+					}
+					
+					double area = (maxx-minx) * (maxy-miny);
+					areacache.put(p, area);
+					return area;
+				}
+			});
+		}
 		
-		Path src = this.src.toPath();
+		Path cut = new Path();
+		for(Path p : sources)
+			makePath(cut, p);
 		
+		return cut;
+	}
+
+	private void makePath(Path cut, Path src) {
 		// Precalculate Z distances
 		double[] zd = new double[src.getSize()];
 		int j=1;
@@ -81,7 +144,6 @@ public class Cut implements PathGenerator {
 		if(dist<ramplen)
 			throw new RenderException(String.format("Path is shorter (%.2f) than ramp length (%.2f)!", dist,ramplen));
 		
-		Path cut = new Path();
 		boolean closedpath = src.isClosed();
 		
 		final double z1 = z0 - depth + passdepth;
@@ -111,23 +173,23 @@ public class Cut implements PathGenerator {
 			}
 		}
 		
-		// Finish
-		z = z0 - depth;
-		j=0;
-		i = src.getSegments().iterator();
-		
-		if(closedpath) {
-			i.next();
-			++j;
+		// Cut the last unfinished part
+		if(finish) {
+			z = z0 - depth;
+			j=0;
+			i = src.getSegments().iterator();
+			
+			if(closedpath) {
+				i.next();
+				++j;
+			}
+			
+			while(i.hasNext()) {
+				Path.Segment s = i.next();
+				cut.addSegment(s.type, s.point.offset(new NumericCoordinate(null, null, z), false, true));
+				if(zd[j]==passdepth)
+					break;
+			}
 		}
-		
-		while(i.hasNext()) {
-			Path.Segment s = i.next();
-			cut.addSegment(s.type, s.point.offset(new NumericCoordinate(null, null, z), false, true));
-			if(zd[j]==passdepth)
-				break;
-		}
-		return cut;
 	}
-
 }
